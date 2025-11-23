7b87ea2d0db289afbaaf535f3db0bfed93b0e0b8
lokeshwaranekambaram <lokesh22905@gmail.com>
2025-11-23 20:18:37 +0530

    Feat : Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 👇 base must match your repo name for GitHub Pages
-  base: '/',
+  base: '/Portfolio/',
   build: {
     outDir: 'dist', // default output folder
     assetsDir: 'assets', // folder for static assets
